83396d697655851d54d2c045c7e38c2af5b795ca
choi718 <hyunmin5655@gmail.com>
2025-08-19 16:21:25 +0900

    진짜 고침

--- front/loc_image.py
+++ front/loc_image.py
@@ -58,16 +58,20 @@ def makepin(img, pt):
 
 # === 측정된 라인 좌표 (map2.png 기준) ===
 line_x = {
-    1: 20,   
-    2: 630,   
-    3: 1270,  
-    4: 1900
+    1: 120,   
+    2: 540,   
+    3: 740,  
+    4: 1180,
+    5: 1370,
+    6: 1820
 }
 line_y_start = {
     1: 250,
     2: 250,
     3: 250,
-    4: 250
+    4: 250,
+    5: 250,
+    6: 250
 }
 
 # === 부스 매핑 ===
@@ -75,17 +79,23 @@ booth = {}
 for i, code in enumerate(["D22","D21","D20","D19","D18","D17","D16","D15","D14","D13","D12","D11","D10","D09"]):
     booth[code] = (1, i, "left")
 codes_line2 = ["D08","D07","D06","D05","D04","D03","D02","D01",
-               "C09","C08","C07","C06","C05","C04","C03","C02","C01",
-               "B43","B42","B41","B40","B39"]
+               "C09","C08","C07","C06","C05","C04"]
+
+codes_line3 =["C03","C02","C01","B43","B42","B41","B40","B39","B38","B37","B36","B35","B34"]
+codes_line4 = ["B33","B32","B31","B30","B29","B28","B27","B26","B25","B24","B23","B22","B21","B20"]
+codes_line5 = ["B19","B18","B17","B16","B15","B14","B13","B12","B11","B10","B09","B08","B07","B06"]
+codes_line6 = ["B05","B04","B03","B02","B01","A10","A09","A08","A07","A06","A05","A04","A03","A02","A01"]
 for i, code in enumerate(codes_line2):
-    booth[code] = (2, i, "left")
-codes_line3 = ["B38","B37","B36","B35","B34","B33","B32","B31","B30","B29","B28","B27","B26","B25","B24","B23","B22","B21","B20",
-               "B19","B18","B17","B16","B15","B14","B13","B12","B11","B10","B09","B08","B07","B06"]
+    booth[code] = (2, i, "right")
 for i, code in enumerate(codes_line3):
-    booth[code] = (3, i, "right")
-codes_line4 = ["B05","B04","B03","B02","B01","A10","A09","A08","A07","A06","A05","A04","A03","A02","A01"]
+    booth[code] = (3, i, "left")
 for i, code in enumerate(codes_line4):
     booth[code] = (4, i, "right")
+
+for i, code in enumerate(codes_line5):
+    booth[code] = (5, i, "left")
+for i, code in enumerate(codes_line6):
+    booth[code] = (6, i, "right")    
 
 aisles = {
     1: 320,
@@ -95,45 +105,40 @@ aisles = {
 
 hx, hy = 732, 1700
 
-def get_aisle_for_booth(line, idx):
-    if line == 1:
+def get_aisle_for_booth(line):
+    if line == 1 or line == 2:
         return 1
-    elif line == 2:
-        if idx <= 13:
-            return 1
-        else:
-            return 2
-    elif line == 3:
-        if idx <= 15:
-            return 2
-        else:
-            return 3
-    elif line == 4:
+    elif line == 3 or line == 4:
+        return 2
+    elif line == 5 or line == 6:
+
         return 3
 
 def get_location_image(team_code):
     edited = deepcopy(background)
     draw = ImageDraw.Draw(edited)
 
-    line, idx, _ = booth[team_code]
-    aisle_idx = get_aisle_for_booth(line, idx)
+    line, idx, direction = booth[team_code]
+    print('line:', line, 'idx:', idx, 'direction:', direction)
+    aisle_idx = get_aisle_for_booth(line)
+    print('aisle_idx:', aisle_idx)
     tx = line_x[line]
-    ty = line_y_start[line] + (idx+1) * blockSize -1860
+    ty = line_y_start[line] + (idx+1) * blockSize
 
     cx, cy = hx, hy + 50
     box_half = 42
     pin_offset = 26
-
-    if aisle_idx < line:
-        tx = tx - box_half - 185
-        pin_x = tx - pin_offset + 140
-        direction = "right"
-    else:
-        tx = tx + box_half + 185
-        pin_x = tx + pin_offset - 140
-        direction = "left"
-
+    pin_x = tx
     aisle_x = aisles[aisle_idx]
+    # if aisle_x < tx:
+    #     tx = tx - box_half - 185
+    #     pin_x = tx - pin_offset + 140
+    #     direction = "right"
+    # else:
+    #     tx = tx + box_half + 185
+    #     pin_x = tx + pin_offset - 140
+    #     direction = "left"
+
     draw.line([(cx, cy), (aisle_x, cy)], fill=(255, 0, 0), width=15)
     draw.line([(aisle_x, cy), (aisle_x, ty)], fill=(255, 0, 0), width=15)
     draw.line([(aisle_x, ty), (tx, ty)], fill=(255, 0, 0), width=15)
@@ -145,6 +150,6 @@ def get_location_image(team_code):
 
 # === 실행 예시 ===
 if __name__ == "__main__":
-    img = get_location_image("B09")
+    img = get_location_image("B06")
     img.show()  # 화면에서 바로 보기
 
